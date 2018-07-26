153d048d0b60346f07340ae61bef4288a3f160b2
Bhagvan Kommadi <bhagvan@crystaldelta.com>
2018-07-26 16:17:55 +0530

    imports wip

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/',include('polls.urls')),
-]
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
