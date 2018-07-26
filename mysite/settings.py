7a3f75e5a73e89a614b69bf83ff1bcb687dd4042
Bhagvan Kommadi <bhagvan@crystaldelta.com>
2018-07-26 15:31:29 +0530

    added wip

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'PORT' : '3306',
         'HOST' : 'localhost',
         'NAME' : 'django_development',
