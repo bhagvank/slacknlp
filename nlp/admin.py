25212d7d17e421629a2fd2f26667ff2b8dea62be
Bhagvan Kommadi <bhagvan@crystaldelta.com>
2018-08-09 18:24:36 +0530

    updated admin.py

--- nlp/admin.py
+++ nlp/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 from .models import SlackUser
 
+ 
+class SlackUserAdmin(admin.ModelAdmin):
+    class Meta:
+        model = SlackUser
+ 
+admin.site.register(SlackUser)
+
 
 
 
