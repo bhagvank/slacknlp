0f0bd696e6bff0b5d6f21a507d8ae3ba71c3f53a
Bhagvan Kommadi <bhagvan@crystaldelta.com>
2018-08-09 17:16:09 +0530

    modified Pipfile and wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+from dj_static import Cling
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+#application = get_wsgi_application()
