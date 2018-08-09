3bc9c1b261158a459cc32e8e5e5efa8189a8541b
Bhagvan Kommadi <bhagvan@crystaldelta.com>
2018-08-09 17:47:40 +0530

    created local_settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 import os
 
+PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
+SITE_ROOT = os.path.dirname(PROJECT_ROOT)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
